d2fe8049ab56964bf26b4ddc98d3d8b66c874da5
Jesse Siliman <jksilliman@gmail.com>
2011-09-23 10:36:27 -0500

    Got working with UChicago LDAP. Fuck yea.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,15 @@
+$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
+require "rvm/capistrano"    
+set :rvm_ruby_string, '1.9.2' # you probably have this already
+set :rvm_type, :user # this is the money config, it defaults to :system
+
+
+set :bundle_roles, :app
+require 'bundler/capistrano'
+default_run_options[:pty] = true
+
 set :application, "committees"
-set :repository,  "github.com/jksilliman/Committees.git"
+set :repository,  "git://github.com/jksilliman/Committees.git"
 set :scm, :git
 set :deploy_via, :remote_cache
 set :deploy_to, "/var/www/committees"
